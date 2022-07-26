a82075834a13a1c7e426d957edd18e346c47caa6
Santosh Kumar <santosh.kumar@nunet.io>
2022-07-26 14:09:11 +0530

    Refactor

--- firecracker/firecracker.go
+++ firecracker/firecracker.go
@@ -13,6 +13,44 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func NewClient(sockFile string) *http.Client {
+	client := &http.Client{
+		Transport: &http.Transport{
+			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
+				return net.Dial("unix", sockFile)
+			},
+		},
+	}
+
+	return client
+}
+
+func MakeRequest(c *gin.Context, client *http.Client, uri string, body []byte, errMsg string) {
+	// set the HTTP method, url, and request body
+	req, err := http.NewRequest(http.MethodPut, uri, bytes.NewBuffer(body))
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"message":   errMsg,
+			"timestamp": time.Now(),
+		})
+		return
+	}
+
+	// set the request header Content-Type for json
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.Header.Set("Accept", "application/json")
+	_, err = client.Do(req)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"message":   errMsg,
+			"timestamp": time.Now(),
+		})
+		return
+	}
+
+}
+
 // InitVM starts the firecracker server for the specific VM. This endpoint requires a socket file.
 // This socket file is further required
 // Further requests are required for configuring the VM.
@@ -50,7 +88,7 @@ func InitVM(c *gin.Context) {
 	// }
 
 	c.JSON(http.StatusOK, gin.H{
-		"message":   "Firecracker VM initiated. Proceed with boot source, fs, and invoke start command",
+		"message":   "VM initiated. Add boot-source, add filesystem, invoke start",
 		"timestamp": time.Now(),
 	})
 
@@ -60,70 +98,21 @@ func BootSource(c *gin.Context) {
 	var jsonStr = []byte(`{"kernel_image_path":"/home/santosh/firecracker/vmlinux.bin", "boot_args": "console=ttyS0 reboot=k panic=1 pci=off"}`)
 
 	// initialize http client
-	client := &http.Client{
-		Transport: &http.Transport{
-			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
-				return net.Dial("unix", "/tmp/firecracker.socket")
-			},
-		},
-	}
+	client := NewClient("/tmp/firecracker.socket")
 
-	// set the HTTP method, url, and request body
-	req, err := http.NewRequest(http.MethodPut, "http://localhost/boot-source", bytes.NewBuffer(jsonStr))
-	if err != nil {
-		c.JSON(400, gin.H{
-			"message":   "Error in making PUT request to /boot-source with give body",
-			"timestamp": time.Now(),
-		})
-		return
-	}
+	errMsg := "Error in making PUT request to /boot-source with give body"
 
-	// set the request header Content-Type for json
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	req.Header.Set("Accept", "application/json")
-	_, err = client.Do(req)
-	if err != nil {
-		c.JSON(400, gin.H{
-			"message":   "Error in making PUT request to /boot-source with give body",
-			"timestamp": time.Now(),
-		})
-		return
-	}
+	MakeRequest(c, client, "http://localhost/boot-source", jsonStr, errMsg)
 }
 
 func Drives(c *gin.Context) {
 	var jsonStr = []byte(`{"drive_id": "rootfs", "path_on_host":"/home/santosh/firecracker/bionic.rootfs.ext4", "is_root_device": true, "is_read_only": false}`)
 
-	// initialize http client
-	client := &http.Client{
-		Transport: &http.Transport{
-			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
-				return net.Dial("unix", "/tmp/firecracker.socket")
-			},
-		},
-	}
+	client := NewClient("/tmp/firecracker.socket")
 
-	// set the HTTP method, url, and request body
-	req, err := http.NewRequest(http.MethodPut, "http://localhost/drives/rootfs", bytes.NewBuffer(jsonStr))
-	if err != nil {
-		c.JSON(400, gin.H{
-			"message":   "Error in making PUT request to /drives with give body",
-			"timestamp": time.Now(),
-		})
-		return
-	}
+	errMsg := "Error in making PUT request to /drives with give body"
 
-	// set the request header Content-Type for json
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	req.Header.Set("Accept", "application/json")
-	_, err = client.Do(req)
-	if err != nil {
-		c.JSON(400, gin.H{
-			"message":   "Error in making PUT request to /drives with give body",
-			"timestamp": time.Now(),
-		})
-		return
-	}
+	MakeRequest(c, client, "http://localhost/drives/rootfs", jsonStr, errMsg)
 
 }
 
@@ -131,105 +120,33 @@ func MachineConfig(c *gin.Context) {
 	var jsonStr = []byte(`{"vcpu_count": 2,"mem_size_mib": 512}`)
 
 	// initialize http client
-	client := &http.Client{
-		Transport: &http.Transport{
-			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
-				return net.Dial("unix", "/tmp.firecracker.socket")
-			},
-		},
-	}
+	client := NewClient("/tmp/firecracker.socket")
 
-	// set the HTTP method, url, and request body
-	req, err := http.NewRequest(http.MethodPut, "http://localhost/machine-config", bytes.NewBuffer(jsonStr))
-	if err != nil {
-		c.JSON(400, gin.H{
-			"message":   "Error in making PUT request to /machine-config with give body",
-			"timestamp": time.Now(),
-		})
-		return
-	}
+	errMsg := "Error in making PUT request to /machine-config with give body"
 
-	// set the request header Content-Type for json
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	req.Header.Set("Accept", "application/json")
-	_, err = client.Do(req)
-	if err != nil {
-		c.JSON(400, gin.H{
-			"message":   "Error in making PUT request to /machine-config with give body",
-			"timestamp": time.Now(),
-		})
-		return
-	}
+	MakeRequest(c, client, "http://localhost/machine-config", jsonStr, errMsg)
+
 }
 
 func NetworkInterfaces(c *gin.Context) {
 	var jsonStr = []byte(`{ "iface_id": "eth0", "guest_mac": "AA:FC:00:00:00:01", "host_dev_name": "tap1" }`)
 
 	// initialize http client
-	client := &http.Client{
-		Transport: &http.Transport{
-			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
-				return net.Dial("unix", "/tmp/firecracker.socket")
-			},
-		},
-	}
+	client := NewClient("/tmp/firecracker.socket")
 
-	// set the HTTP method, url, and request body
-	req, err := http.NewRequest(http.MethodPut, "http://localhost/network-interfaces/eth0", bytes.NewBuffer(jsonStr))
-	if err != nil {
-		panic(err)
-		// c.JSON(400, gin.H{
-		// 	"message":   "Error in making PUT request to /network-interfaces with give body",
-		// 	"timestamp": time.Now(),
-		// })
-		// return
-	}
+	errMsg := "Error in making PUT request to /network-interfaces with give body"
 
-	// set the request header Content-Type for json
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	req.Header.Set("Accept", "application/json")
-	_, err = client.Do(req)
-	if err != nil {
-		panic(err)
-		// c.JSON(400, gin.H{
-		// 	"message":   "Error in making PUT request to /network-interfaces with give body",
-		// 	"timestamp": time.Now(),
-		// })
-		// return
-	}
+	MakeRequest(c, client, "http://localhost/network-interfaces/eth0", jsonStr, errMsg)
 }
 
 func Actions(c *gin.Context) {
 	var jsonStr = []byte(`{"action_type": "InstanceStart"}`)
 
 	// initialize http client
-	client := &http.Client{
-		Transport: &http.Transport{
-			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
-				return net.Dial("unix", "/tmp/firecracker.socket")
-			},
-		},
-	}
+	client := NewClient("/tmp/firecracker.socket")
 
-	// set the HTTP method, url, and request body
-	req, err := http.NewRequest(http.MethodPut, "http://localhost/actions", bytes.NewBuffer(jsonStr))
-	if err != nil {
-		c.JSON(400, gin.H{
-			"message":   "Error in making PUT request to /actions with give body",
-			"timestamp": time.Now(),
-		})
-		return
-	}
+	errMsg := "Error in making PUT request to /actions with give body"
 
-	// set the request header Content-Type for json
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	req.Header.Set("Accept", "application/json")
-	_, err = client.Do(req)
-	if err != nil {
-		c.JSON(400, gin.H{
-			"message":   "Error in making PUT request to /actions with give body",
-			"timestamp": time.Now(),
-		})
-		return
-	}
+	MakeRequest(c, client, "http://localhost/actions", jsonStr, errMsg)
+
 }
