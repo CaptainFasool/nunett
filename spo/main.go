cc038d91bbb5e509dd390152a74afc0365e69aee
Santosh Kumar <santosh.kumar@nunet.io>
2022-08-18 18:00:18 +0530

    Add placeholder handlers

---
+++ spo/main.go
@@ -0,0 +1,16 @@
+package spo
+
+import "github.com/gin-gonic/gin"
+
+// SearchDevice searches the DHT for non-busy, available devices with "allow_cardano" metadata. Search results returns a list of available devices along with the resource capacity.
+func SearchDevice(c *gin.Context) {
+
+}
+
+func DeployAuto(c *gin.Context) {
+
+}
+
+func DeployManual(c *gin.Context) {
+
+}
