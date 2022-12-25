e44184e9f0a1b6592e21e2aed9fa4318a25f35ca
Dagim Sisay <dagiopia@gmail.com>
2022-12-25 22:41:21 +0000

    Oneline script installation by scryotic

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 )
 
 // @title           Device Management Service
-// @version         0.4.16
+// @version         0.4.17
 // @description     A dashboard application for computing providers.
 // @termsOfService  https://nunet.io/tos
 
