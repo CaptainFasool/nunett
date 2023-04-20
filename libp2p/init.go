33af1cb1ab42df8b06a5fe6c8a32665587a8dc45
Tewodros Kederalah <teddylnk1@gmail.com>
2023-04-20 12:59:19 +0300

    revert bootstrap peers

--- libp2p/init.go
+++ libp2p/init.go
@@ -49,7 +49,12 @@ var NuNetBootstrapPeers []multiaddr.Multiaddr
 
 func init() {
 	for _, s := range []string{
-        "/ip4/5.161.142.32/tcp/6763/p2p/QmZLHBqTYbu9PKmhnGDkFGgMZaF8B9eZG9YSSCVpkdr7kK",
+		"/dnsaddr/bootstrap.p2p.nunet.io/p2p/QmQ2irHa8aFTLRhkbkQCRrounE4MbttNp8ki7Nmys4F9NP",
+		"/dnsaddr/bootstrap.p2p.nunet.io/p2p/Qmf16N2ecJVWufa29XKLNyiBxKWqVPNZXjbL3JisPcGqTw",
+		"/dnsaddr/bootstrap.p2p.nunet.io/p2p/QmTkWP72uECwCsiiYDpCFeTrVeUM9huGTPsg3m6bHxYQFZ",
+		// libp2p bootstrap nodes as fallback
+		"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+		"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
 	} {
 		ma, err := multiaddr.NewMultiaddr(s)
 		if err != nil {
