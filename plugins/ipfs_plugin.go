1a2927e9935ac7ccd9e5950020d5df0ca13e454b
JPexplorer <jpexplorer@pop-os.localdomain>
2023-05-31 21:54:34 -0300

    feat/refact: isolating init.go, function to get slice of enabledPlugins, plugins interface type

--- plugins/ipfs_plugin.go
+++ plugins/ipfs_plugin.go
@@ -5,16 +5,20 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/go-connections/nat"
 )
 
-var (
+type IPFSPlugin struct{}
+
+const (
 	ipfsPluginImg = "registry.gitlab.com/nunet/data-persistence/ipfs-plugin:0.0.1"
 )
 
-func RunContainer() {
+func (p *IPFSPlugin) start(errCh chan error) {
 	err := dockerDMS.PullImage(ipfsPluginImg)
 	if err != nil {
 		zlog.Sugar().Errorf("Couldn't pull ipfs-plugin image: %v", err)
+		errCh <- err
 		return
 	}
 
@@ -22,20 +26,37 @@ func RunContainer() {
 
 	containerConfig := &container.Config{
 		Image: ipfsPluginImg,
+		ExposedPorts: nat.PortSet{
+			"31001/tcp": struct{}{},
+		},
 	}
 
-	resp, err := dc.ContainerCreate(ctx, containerConfig, nil, nil, nil, "")
+	// hostConfig := &container.HostConfig{
+	// 	PortBindings: nat.PortMap{
+	// 		"31001/tcp": []nat.PortBinding{
+	// 			{
+	// 				HostIP:   "127.0.0.1",
+	// 				HostPort: "31001",
+	// 			},
+	// 		},
+	// 	},
+	// }
 
+	resp, err := dc.ContainerCreate(ctx, containerConfig, nil, nil, nil, "")
 	if err != nil {
 		zlog.Sugar().Errorf("Unable to create plugin container: %v", err)
+		errCh <- err
 		return
 	}
 
 	if err := dc.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
 		zlog.Sugar().Errorf("Unable to start plugin container: %v", err)
+		errCh <- err
 		return
 	}
 
 	// statusCh, errCh := dc.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	errCh <- nil
+	return
 
 }
