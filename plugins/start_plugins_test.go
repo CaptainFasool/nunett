afabc58ecd35c31e0f7f5f2038e71c6cb8694758
JPexplorer <jpexplorer@pop-os.localdomain>
2023-06-26 12:49:56 -0300

    feat/refact(Plugins): testing SolveEnabledFunctions and refacting mock data and other tests

--- plugins/start_plugins_test.go
+++ plugins/start_plugins_test.go
@@ -6,37 +6,62 @@ import (
 
 	"gitlab.com/nunet/device-management-service/models"
 	"gitlab.com/nunet/device-management-service/plugins/ipfs_plugin"
+	"gitlab.com/nunet/device-management-service/utils"
 )
 
-func TestGetPluginType(t *testing.T) {
-	tests := []struct {
-		testName        string
-		pluginName      string
-		expectedErr     bool
-		pluginInterface reflect.Type
-	}{
-		{
-			testName:        "ipfs-plugin",
+type expectedMockPlugin struct {
+	pluginName      string
+	expectedErr     bool
+	pluginInterface reflect.Type
+}
+
+func getMockExpectedPluginsTable() map[string]expectedMockPlugin {
+	return map[string]expectedMockPlugin{
+		"ipfs-plugin": {
 			pluginName:      "ipfs-plugin",
 			expectedErr:     false,
 			pluginInterface: reflect.TypeOf(&ipfs_plugin.IPFSPlugin{}),
 		},
-		{
-			testName:        "unknown-plugin",
+		"unknown-plugin": {
 			pluginName:      "unknown-plugin",
 			expectedErr:     true,
 			pluginInterface: nil,
 		},
-		{
-			testName:        "empty string",
+		"empty-string": {
 			pluginName:      "",
 			expectedErr:     true,
 			pluginInterface: nil,
 		},
 	}
+}
 
-	for _, tt := range tests {
-		t.Run(tt.testName, func(t *testing.T) {
+func getMockExpectedPluginsNames() []string {
+	pluginsTable := getMockExpectedPluginsTable()
+	var pluginsNames []string
+	for _, pluginTestInfo := range pluginsTable {
+		if len(pluginTestInfo.pluginName) == 0 {
+			continue
+		}
+		pluginsNames = append(pluginsNames, pluginTestInfo.pluginName)
+	}
+	return pluginsNames
+}
+
+func retrieveEnabledPluginsFromMock() []reflect.Type {
+	pluginsTable := getMockExpectedPluginsTable()
+	var plugins []reflect.Type
+	for _, pluginInfo := range pluginsTable {
+		if pluginInfo.pluginInterface != nil {
+			plugins = append(plugins, pluginInfo.pluginInterface)
+		}
+	}
+	return plugins
+}
+
+func TestGetPluginType(t *testing.T) {
+	pluginsTable := getMockExpectedPluginsTable()
+	for testName, tt := range pluginsTable {
+		t.Run(testName, func(t *testing.T) {
 			pluginInterface, err := getPluginType(tt.pluginName)
 			if reflect.TypeOf(pluginInterface) != tt.pluginInterface {
 				t.Errorf("getPluginType() = %v, want %v", reflect.TypeOf(pluginInterface), tt.pluginInterface)
@@ -50,15 +75,15 @@ func TestGetPluginType(t *testing.T) {
 	}
 }
 
+func mockReadMetadataFile() (models.MetadataV2, error) {
+	expectedPlugins := getMockExpectedPluginsNames()
+	return models.MetadataV2{
+		Plugins: expectedPlugins,
+	}, nil
+}
+
 func TestGetMetadataPlugins(t *testing.T) {
-	expectedPlugins := []string{"ipfs-plugin", "test-plugin"}
-
-	mockReadMetadataFile := func() (models.MetadataV2, error) {
-		return models.MetadataV2{
-			Plugins: expectedPlugins,
-		}, nil
-	}
-
+	expectedPlugins := getMockExpectedPluginsNames()
 	plugins, err := getMetadataPlugins(mockReadMetadataFile)
 
 	if err != nil {
@@ -69,9 +94,44 @@ func TestGetMetadataPlugins(t *testing.T) {
 		t.Errorf("Wanted %d plugins, received %d plugins", len(expectedPlugins), len(plugins))
 	}
 
-	for i, plugin := range plugins {
-		if expectedPlugins[i] != plugin {
-			t.Errorf("Wanted plugin: %s, received: %s", expectedPlugins[i], plugin)
+	if !utils.AreSlicesEqual(plugins, expectedPlugins) {
+		t.Errorf("Wanted plugins: %s, received plugins: %s", expectedPlugins, plugins)
+	}
+}
+
+func TestSolveEnabledPlugins(t *testing.T) {
+	expectedEnabledPlugins := retrieveEnabledPluginsFromMock()
+
+	plugins, err := solveEnabledPlugins(mockReadMetadataFile)
+
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if len(plugins) != len(expectedEnabledPlugins) {
+		t.Errorf("Wanted %d plugins, received %d plugins", len(expectedEnabledPlugins), len(plugins))
+	}
+
+	// Transforming plugins to reflect.Type to be compared with expected plugins
+	var pluginsByType []reflect.Type
+	for _, pluginInterface := range plugins {
+		pluginsByType = append(pluginsByType, reflect.TypeOf(pluginInterface))
+	}
+
+	// Checking if we got the expected plugins types
+	// TODO: maybe improve this part
+	matchedPlugins := 0
+OuterLoop:
+	for _, expectedPlugin := range expectedEnabledPlugins {
+		for _, enabledPlugin := range plugins {
+			if reflect.TypeOf(enabledPlugin) == expectedPlugin {
+				matchedPlugins++
+				continue OuterLoop
+			}
 		}
+	}
+
+	if matchedPlugins != len(expectedEnabledPlugins) {
+		t.Errorf("got %v, want %v", plugins, expectedEnabledPlugins)
 	}
 }
