505c7c536785cc3782f38eeee0c9ef602563a997
JPexplorer <jpexplorer@pop-os.localdomain>
2023-06-27 11:07:11 -0300

    feat(filter): unit test to filterByNeededPlugins function

--- libp2p/machines/filter_test.go
+++ libp2p/machines/filter_test.go
@@ -37,8 +37,73 @@ func TestIsIPFSPluginNeeded(t *testing.T) {
 
 }
 
-func getMockDepReq(pluginIPFSFunctionalities []string) models.DeploymentRequest {
+func getMockDepReq(additionalFeats []string) models.DeploymentRequest {
 	var depReq models.DeploymentRequest
-	depReq.Params.AdditionalFeatures = pluginIPFSFunctionalities
+	depReq.Params.AdditionalFeatures = additionalFeats
 	return depReq
+}
+
+func TestFilterByNeededPlugins(t *testing.T) {
+	// TODO: this test needs improvement but it's because
+	// the original function itself needs to be more dynamic
+	peerOne := models.PeerData{
+		EnabledPlugins: []string{"ipfs-plugin", "other-plugin"},
+		PeerID:         "peerOne",
+	}
+
+	peerTwo := models.PeerData{
+		EnabledPlugins: []string{"ipfs-plugin"},
+		PeerID:         "peerTwo",
+	}
+
+	peerThree := models.PeerData{
+		EnabledPlugins: []string{},
+		PeerID:         "peerThree",
+	}
+
+	testCases := []struct {
+		name           string
+		peers          []models.PeerData
+		depReqAddFeats []string
+		expectedResult []models.PeerData
+	}{
+		{
+			name:           "Test with one plugin needed",
+			peers:          []models.PeerData{peerOne, peerTwo, peerThree},
+			depReqAddFeats: []string{"outputIPFS"},
+			expectedResult: []models.PeerData{peerOne, peerTwo},
+		},
+		{
+			name:           "Test with no plugin needed",
+			peers:          []models.PeerData{peerOne, peerTwo, peerThree},
+			depReqAddFeats: []string{},
+			expectedResult: []models.PeerData{peerOne, peerTwo, peerThree},
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			depReq := getMockDepReq(tt.depReqAddFeats)
+			result := filterByNeededPlugins(tt.peers, depReq)
+
+			if len(result) != len(tt.expectedResult) {
+				t.Errorf("wanted %d peers, got %d peers", len(result), len(tt.expectedResult))
+			}
+
+			matchedPeers := 0
+		OuterLoop:
+			for _, peer := range result {
+				for _, expectedPeer := range tt.expectedResult {
+					if peer.PeerID == expectedPeer.PeerID {
+						matchedPeers++
+						continue OuterLoop
+					}
+				}
+			}
+
+			if matchedPeers != len(tt.expectedResult) {
+				t.Errorf("got %v, want %v", result, tt.expectedResult)
+			}
+		})
+	}
 }
