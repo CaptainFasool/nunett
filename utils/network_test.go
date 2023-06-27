4e89fecf0540c0ecea6848f53fb18c8da2ddf3c2
Gustavo Silva <gustavo.silva@nunet.io>
2023-06-27 09:38:47 -0300

    test: add more cases for unit test network_test.go

--- utils/network_test.go
+++ utils/network_test.go
@@ -1,13 +1,19 @@
 package utils
 
 import (
-	"bytes"
+	"io"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
+
+type Info struct {
+	Version string `json: "version"`
+}
 
 func TestGetInternalBaseURL(t *testing.T) {
 	// Test with a valid internal endpoint
@@ -30,29 +36,50 @@ func TestGetInternalBaseURL(t *testing.T) {
 
 func TestMakeInternalRequest(t *testing.T) {
 	// Test with a valid request
-	body := []byte(`{"version": "0.4.97"}`)
-	req := httptest.NewRequest("GET", "/swagger/doc.json", bytes.NewBuffer(body))
+	req := httptest.NewRequest("GET", "/swagger/doc.json", nil)
 	w := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(w)
 	c.Request = req
 
-	resp, err := MakeInternalRequest(c, "GET", "/swagger/doc.json", body)
+	resp, err := MakeInternalRequest(c, "GET", "/swagger/doc.json", nil)
 	if err != nil {
 		t.Errorf("Expected no error, but got %v", err)
 	}
 	defer resp.Body.Close()
 
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Error reading response body: %v", err)
+	}
+
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("Expected status code %d, but got %d", http.StatusOK, resp.StatusCode)
 	}
+
+	expectedContentType := "application/json; charset=utf-8"
+	if resp.Header.Get("Content-Type") != expectedContentType {
+		t.Errorf("Expected Content-Type header %s, but got %s", expectedContentType, resp.Header.Get("Content-Type"))
+	}
+
+	var info Info
+	err = json.Unmarshal(body, &info)
+	if err != nil {
+		t.Errorf("Error unmarshaling response body: %v", err)
+	}
+
+	expectedInfo := "0.4.97"
+	fmt.Println(info)
+	if expectedInfo != info.Version {
+		t.Errorf("Expected key %s, but got %s", expectedInfo, info.Version)
+	}
+
 	// Test with an invalid internal endpoint
-	body = []byte(`{"version": "0.4.97"}`)
-	req = httptest.NewRequest("GET", "/swagger/doc.json", bytes.NewBuffer(body))
+	req = httptest.NewRequest("GET", "/swagger/doc.json", nil)
 	w = httptest.NewRecorder()
 	c, _ = gin.CreateTestContext(w)
 	c.Request = req
 
-	resp, err = MakeInternalRequest(c, "GET", "", body)
+	resp, err = MakeInternalRequest(c, "GET", "", nil)
 	if err == nil {
 		t.Errorf("Expected an error, but got none")
 	}
