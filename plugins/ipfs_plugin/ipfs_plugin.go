8835a28851acca6fa73444f068ce7f955a31e65a
JPexplorer <jpexplorer@pop-os.localdomain>
2023-07-04 12:44:05 -0300

    feat: new go routine to manage plugins through different channels

--- plugins/ipfs_plugin/ipfs_plugin.go
+++ plugins/ipfs_plugin/ipfs_plugin.go
@@ -7,6 +7,8 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/go-connections/nat"
+	"gitlab.com/nunet/device-management-service/models"
+	"gitlab.com/nunet/device-management-service/plugins/plugins_management"
 )
 
 type IPFSPlugin struct{}
@@ -22,43 +24,45 @@ func (p *IPFSPlugin) OnboardedName() string {
 	return pluginName
 }
 
-func (p *IPFSPlugin) Start(errCh chan error) {
+func (p *IPFSPlugin) Start(pluginsManager *plugins_management.PluginsInfoChannels) {
 	err := PullImage(ipfsPluginImg)
 	if err != nil {
 		zlog.Sugar().Errorf("Couldn't pull ipfs-plugin docker image: %v", err)
-		errCh <- err
+		pluginsManager.ErrCh <- err
 		return
 	}
 
-	zlog.Info("Entering IPFS-Plugin container")
+	zlog.Sugar().Debug("Entering IPFS-Plugin container")
 
 	containerConfig, hostConfig, err := configureContainer(ipfsPluginImg, portDc, addrDc, port)
 	if err != nil {
 		zlog.Sugar().Errorf("Error occured when configuring container: %v", err)
-		errCh <- err
+		pluginsManager.ErrCh <- err
 		return
 	}
 
 	resp, err := dc.ContainerCreate(ctx, containerConfig, hostConfig, nil, nil, "")
 	if err != nil {
 		zlog.Sugar().Errorf("Unable to create plugin container: %v", err)
-		errCh <- err
+		pluginsManager.ErrCh <- err
 		return
 	}
 
-	zlog.Info("Starting IPFS-Plugin container")
+	zlog.Sugar().Debug("Starting IPFS-Plugin container")
 	if err := dc.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
 		zlog.Sugar().Errorf("Unable to start plugin container: %v", err)
-		errCh <- err
+		pluginsManager.ErrCh <- err
 		return
 	}
+
+    make(models.FreeResources)
 
 	// statusCh, errCh := dc.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
 	// TODO: update DHT of usage of resources
-	errCh <- nil
-	return
 }
 
+// configureContainer return configuration structs to start a Docker container. Here is where
+// ports, imageURL and other Docker container configs are defined.
 func configureContainer(img string, exposedPort, hostIP, hostPort string) (*container.Config, *container.HostConfig, error) {
 	port, err := nat.NewPort("tcp", exposedPort)
 	if err != nil {
