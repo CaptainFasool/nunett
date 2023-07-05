d51a69f9d3bd16d37873b536d95f52544e17992b
Raynner Carvalho <raynner.carvalho@nunet.io>
2023-07-05 16:16:45 +0000

    Resolve "When using floats instead of integers, 'nunet onboard' ignores capacity and successfully onboards devices."

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 )
 
 // @title           Device Management Service
-// @version         0.4.103
+// @version         0.4.104
 // @description     A dashboard application for computing providers.
 // @termsOfService  https://nunet.io/tos
 
