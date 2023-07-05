c4c571bf7716997c3064a3ce2306c0c98624e701
Avimanyu Bandyopadhyay <avimanyu786@gmail.com>
2023-07-05 14:31:48 +0000

    Fix nunet capacity --gpu-status command

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 )
 
 // @title           Device Management Service
-// @version         0.4.98
+// @version         0.4.99
 // @description     A dashboard application for computing providers.
 // @termsOfService  https://nunet.io/tos
 
