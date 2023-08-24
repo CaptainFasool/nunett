5673dc4d8a4e712f53fc842d46bc0b2415c7d89e
Gustavo Silva <gustavo.silva@nunet.io>
2023-08-24 16:45:52 -0300

    fix(libp2p): fix typo in welcome message

--- libp2p/handler.go
+++ libp2p/handler.go
@@ -271,7 +271,7 @@ func StartChatHandler(c *gin.Context) {
 		return
 	}
 
-	welcomeMessage := fmt.Sprintf("Enter the message that you wish to send to %s with strea ID: %s and press return.", peerID, stream.ID())
+	welcomeMessage := fmt.Sprintf("Enter the message that you wish to send to %s with stream ID: %s and press return.", peerID, stream.ID())
 
 	err = ws.WriteMessage(websocket.TextMessage, []byte(welcomeMessage))
 	if err != nil {
