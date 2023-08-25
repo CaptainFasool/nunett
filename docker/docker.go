1489b98b1fda85002ee2d626db48e41a1af4655f
avimanyu786 <avimanyu786@gmail.com>
2023-08-25 18:00:34 +0530

    customizing for amdgpu

--- docker/docker.go
+++ docker/docker.go
@@ -37,7 +37,7 @@ import (
 var (
 	vcpuToMicroseconds float64       = 100000
 	logUpdateInterval time.Duration = time.Duration(config.GetConfig().Job.LogUpdateInterval) * time.Minute
-	spdUpdateInterval = 1 * time.Second
+	spdUpdateInterval = 5 * time.Second
 )
 
 func freeUsedResources() {
