7c8e4b7dcfaa5a623d82d720b3f5f474d884137a
Gustavo Silva <gustavo.silva@nunet.io>
2023-08-28 10:17:55 -0300

    refactor(cli): reuse methods from WS client on both startChat and joinChat commands

--- cmd/join_chat.go
+++ cmd/join_chat.go
@@ -1,18 +1,11 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"net/url"
 	"os"
-	"os/signal"
-	"strconv"
-	"time"
 
-	"github.com/gorilla/websocket"
 	"github.com/spf13/cobra"
-	"gitlab.com/nunet/device-management-service/internal/config"
+	"gitlab.com/nunet/device-management-service/utils"
 )
 
 func init() {
@@ -20,68 +13,27 @@ func init() {
 
 var joinChatCmd = &cobra.Command{
 	Use:   "chat",
-	Short: "",
+	Short: "Join open chat stream",
 	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
-		interrupt := make(chan os.Signal, 1)
-		signal.Notify(interrupt, os.Interrupt)
+		query := "streamID=" + args[0]
 
-		streamID := args[0]
-		port := config.GetConfig().Rest.Port
-		portStr := strconv.Itoa(port)
-
-		serverURL := url.URL{
-			Scheme:   "ws",
-			Host:     "localhost:" + portStr,
-			Path:     "/api/v1/peers/chat/join",
-			RawQuery: "streamID=" + streamID,
-		}
-
-		conn, _, err := websocket.DefaultDialer.Dial(serverURL.String(), nil)
+		joinURL, err := utils.InternalAPIURL("ws", "/api/v1/peers/chat/join", query)
 		if err != nil {
-			log.Fatalf("Could not connect: %v", err)
+			fmt.Println(err)
+			os.Exit(1)
 		}
-		defer conn.Close()
 
-		// Goroutine for receiving messages
-		go func() {
-			for {
-				_, msg, err := conn.ReadMessage()
-				if err != nil {
-					log.Fatalf("Error trying to read message: %v", err)
-				}
-				fmt.Println(string(msg))
-			}
-		}()
-
-		// Goroutine for sending messages
-		go func() {
-			scanner := bufio.NewScanner(os.Stdin)
-			for scanner.Scan() {
-				text := scanner.Text()
-				if err := conn.WriteMessage(websocket.TextMessage, []byte(text)); err != nil {
-					log.Println("Error while sending message:", err)
-					return
-				}
-			}
-			if scanner.Err() != nil {
-				log.Println("Error while reading from stdin:", scanner.Err())
-			}
-		}()
-
-		ticker := time.NewTicker(30 * time.Second)
-		defer ticker.Stop()
-
-		// Define ping-pong mechanism
-		for {
-			select {
-			case <-ticker.C:
-				conn.WriteMessage(websocket.PingMessage, []byte{})
-			case <-interrupt:
-				log.Println("Received interrupt signal, closing connection")
-				conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-				return
-			}
+		client := &Client{}
+		err = client.Initialize(joinURL)
+		if err != nil {
+			fmt.Println("Failed to initialize client:", err)
+			os.Exit(1)
 		}
+		defer client.Conn.Close()
+
+		go client.ReadMessages()
+		go client.WriteMessages()
+		client.HandleInterruptsAndPings()
 	},
 }
--- cmd/start_chat.go
+++ cmd/start_chat.go
@@ -1,18 +1,11 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"net/url"
 	"os"
-	"os/signal"
-	"strconv"
-	"time"
 
-	"github.com/gorilla/websocket"
 	"github.com/spf13/cobra"
-	"gitlab.com/nunet/device-management-service/internal/config"
+	"gitlab.com/nunet/device-management-service/utils"
 )
 
 func init() {
@@ -24,65 +17,24 @@ var startChatCmd = &cobra.Command{
 	Short: "Start chat with a peer",
 	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
-		interrupt := make(chan os.Signal, 1)
-		signal.Notify(interrupt, os.Interrupt)
+		query := "peerID=" + args[0]
 
-		peerID := args[0]
-		port := config.GetConfig().Rest.Port
-		portStr := strconv.Itoa(port)
-
-		serverURL := url.URL{
-			Scheme:   "ws",
-			Host:     "localhost:" + portStr,
-			Path:     "/api/v1/peers/chat/start",
-			RawQuery: "peerID=" + peerID,
-		}
-
-		conn, _, err := websocket.DefaultDialer.Dial(serverURL.String(), nil)
+		startURL, err := utils.InternalAPIURL("ws", "/api/v1/peers/chat/start", query)
 		if err != nil {
-			log.Fatalf("Could not connect: %v", err)
+			fmt.Println(err)
+			os.Exit(1)
 		}
-		defer conn.Close()
 
-		// Goroutine for receiving messages
-		go func() {
-			for {
-				_, msg, err := conn.ReadMessage()
-				if err != nil {
-					log.Fatalf("Error trying to read message: %v", err)
-				}
-				fmt.Println(string(msg))
-			}
-		}()
-
-		// Goroutine for sending messages
-		go func() {
-			scanner := bufio.NewScanner(os.Stdin)
-			for scanner.Scan() {
-				text := scanner.Text()
-				if err := conn.WriteMessage(websocket.TextMessage, []byte(text)); err != nil {
-					log.Println("Error while sending message:", err)
-					return
-				}
-			}
-			if scanner.Err() != nil {
-				log.Println("Error while reading from stdin:", scanner.Err())
-			}
-		}()
-
-		ticker := time.NewTicker(30 * time.Second)
-		defer ticker.Stop()
-
-		// Define ping-pong mechanism
-		for {
-			select {
-			case <-ticker.C:
-				conn.WriteMessage(websocket.PingMessage, []byte{})
-			case <-interrupt:
-				log.Println("(interrupt) closing connection")
-				conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-				return
-			}
+		client := &Client{}
+		err = client.Initialize(startURL)
+		if err != nil {
+			fmt.Println("Failed to initialize client:", err)
+			os.Exit(1)
 		}
+		defer client.Conn.Close()
+
+		go client.ReadMessages()
+		go client.WriteMessages()
+		client.HandleInterruptsAndPings()
 	},
 }
