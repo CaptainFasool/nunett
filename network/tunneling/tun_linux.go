d7d8c8c2598008fe1887002c1d77ea939a0313e3
JPexplorer <jpexplorer@pop-os.localdomain>
2023-11-08 17:19:58 -0300

    refact(tunneling): improving error handling and creating new function that merges two other functions

--- network/tunneling/tun_linux.go
+++ network/tunneling/tun_linux.go
@@ -5,6 +5,7 @@ package tunneling
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/songgao/water"
 	"github.com/vishvananda/netlink"
@@ -77,16 +78,33 @@ func (t *TUN) Up() error {
 func (t *TUN) Down() error {
 	link, err := netlink.LinkByName(t.Iface.Name())
 	if err != nil {
-		return err
+		return fmt.Errorf("Couldn't find interface: %w", err)
 	}
-	return netlink.LinkSetDown(link)
+
+	if err := netlink.LinkSetDown(link); err != nil {
+		return fmt.Errorf("Couldn't bring down interface: %w", err)
+	}
+	return nil
 }
 
 // Delete removes a TUN device from the host.
-func Delete(name string) error {
-	link, err := netlink.LinkByName(name)
+func (t *TUN) Delete() error {
+	link, err := netlink.LinkByName(t.Iface.Name())
 	if err != nil {
+		return fmt.Errorf("Couldn't find interface: %w", err)
+
+	}
+	if err := netlink.LinkDel(link); err != nil {
+		return fmt.Errorf("Couldn't delete interface: %w", err)
+	}
+	return nil
+}
+
+// SetDownAndDelete stops active connections and delete
+// the TUN device from the host.
+func (t *TUN) SetDownAndDelete() error {
+	if err := t.Down(); err != nil {
 		return err
 	}
-	return netlink.LinkDel(link)
+	return t.Delete()
 }
