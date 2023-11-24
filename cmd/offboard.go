58b9f2d8959a71117de4d8d85887d2ca231887f7
Gustavo Silva <gustavo.silva@nunet.io>
2023-11-24 18:07:40 -0300

    refactor(cmd): DI for offboard cmd

--- cmd/offboard.go
+++ cmd/offboard.go
@@ -8,65 +8,73 @@ import (
 	"gitlab.com/nunet/device-management-service/utils"
 )
 
-var flagForce bool
+var (
+	flagForce bool
 
-func init() {
-	offboardCmd.Flags().BoolVarP(&flagForce, "force", "f", false, "force offboarding")
-}
+	offboardCmd = NewOffboardCmd(&Utils{})
+)
 
-var offboardCmd = &cobra.Command{
-	Use:    "offboard",
-	Short:  "Offboard the device from NuNet",
-	PreRun: isDMSRunning(),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		onboarded, err := utils.IsOnboarded()
-		if err != nil {
-			return fmt.Errorf("could not check onboard status: %w", err)
-		}
+func NewOffboardCmd(util Utility) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:    "offboard",
+		Short:  "Offboard the device from NuNet",
+		PreRun: isDMSRunning(),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var (
+				onboarded bool
+				confirmed bool
 
-		if !onboarded {
-			return fmt.Errorf("looks like your machine is not onboarded")
-		}
+				force  bool
+				query  string
+				body   []byte
+				errMsg string
+				msg    string
 
-		fmt.Fprintln(cmd.OutOrStdout(), "Warning: Offboarding will remove all your data and you will not be able to onboard again with the same identity")
-		answer := utils.PromptYesNo("Are you sure you want to offboard? (y/N)")
-		if !answer {
-			return fmt.Errorf("offboard aborted by user")
-		} else {
-			force, _ := cmd.Flags().GetBool("force")
-			if !force {
-				body, err := utils.ResponseBody(nil, "DELETE", "/api/v1/onboarding/offboard", "", nil)
-				if err != nil {
-					return fmt.Errorf("unable to fetch response body: %w", err)
-				}
+				err error
+			)
 
-				if errMsg, err := jsonparser.GetString(body, "error"); err == nil { // if field "error" IS found
-					return fmt.Errorf("got error response from server: %w", errMsg)
-				} else if err == jsonparser.KeyPathNotFoundError { // if field "error" is NOT found
-					msg, _ := jsonparser.GetString(body, "message")
-					fmt.Fprintln(cmd.OutOrStdout(), msg)
-				} else { // if another error occurred
-					return fmt.Errorf("could not parse string response: %w", err)
-				}
-			} else {
-				query := fmt.Sprintf("force=%t", force)
-
-				body, err := utils.ResponseBody(nil, "DELETE", "/api/v1/onboarding/offboard", query, nil)
-				if err != nil {
-					return fmt.Errorf("unable to fetch response body: %w", err)
-				}
-
-				if errMsg, err := jsonparser.GetString(body, "error"); err == nil { // if field "error" IS found
-					return fmt.Errorf("got error response from server: %w", errMsg)
-				} else if err == jsonparser.KeyPathNotFoundError { // if field "error" is NOT found
-					msg, _ := jsonparser.GetString(body, "message")
-					fmt.Fprintln(cmd.OutOrStdout(), msg)
-				} else { // if another error occurred
-					return fmt.Errorf("could not parse string response: %w", err)
-				}
+			onboarded, err = util.IsOnboarded()
+			if err != nil {
+				return fmt.Errorf("could not check onboard status: %w", err)
 			}
-		}
+			if !onboarded {
+				return fmt.Errorf("looks like your machine is not onboarded")
+			}
 
-		return nil
-	},
+			fmt.Fprintln(cmd.OutOrStdout(), "Warning: Offboarding will remove all your data and you will not be able to onboard again with the same identity")
+
+			confirmed = utils.PromptYesNo("Are you sure you want to offboard? (y/N)")
+			if !confirmed {
+				return fmt.Errorf("offboard aborted by user")
+			}
+
+			force, _ = cmd.Flags().GetBool("force")
+			if force {
+				query = fmt.Sprintf("force=%t", force)
+			}
+
+			body, err = util.ResponseBody(nil, "DELETE", "/api/v1/onboarding/offboard", query, nil)
+			if err != nil {
+				return fmt.Errorf("unable to fetch response body: %w", err)
+			}
+
+			errMsg, err = jsonparser.GetString(body, "error")
+			if err == nil {
+				return fmt.Errorf("got error response from server: %w", errMsg)
+			} else if err != jsonparser.KeyPathNotFoundError {
+				return fmt.Errorf("could not parse string response: %w", err)
+			}
+
+			msg, err = jsonparser.GetString(body, "message")
+			if err != nil {
+				return fmt.Errorf("failed to get message string from response body: %w", err)
+			}
+			fmt.Fprintln(cmd.OutOrStdout(), msg)
+
+			return nil
+		},
+	}
+	cmd.Flags().BoolVarP(&flagForce, "force", "f", false, "force offboarding")
+
+	return cmd
 }
