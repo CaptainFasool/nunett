0e6fd21b1c1d508f6d71e46685341ca57df2d3cb
Gustavo Silva <gustavo.silva@nunet.io>
2023-11-24 18:07:06 -0300

    refactor(cmd): inject dependencies in gpu_capacity.go

--- cmd/gpu_capacity.go
+++ cmd/gpu_capacity.go
@@ -26,119 +26,150 @@ type ContainerOptions struct {
 	Entrypoint []string
 }
 
-var flagCudaTensor, flagRocmHip bool
+var (
+	flagCudaTensor, flagRocmHip bool
+)
 
-func init() {
-	gpuCapacityCmd.Flags().BoolVarP(&flagCudaTensor, "cuda-tensor", "c", false, "check CUDA Tensor")
-	gpuCapacityCmd.Flags().BoolVarP(&flagRocmHip, "rocm-hip", "r", false, "check ROCM-HIP")
+func NewGPUCapacityCmd(librarier Librarier, docker Docker) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:    "capacity",
+		Short:  "Check availability of NVIDIA/AMD GPUs",
+		PreRun: isDMSRunning(),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cuda, _ := cmd.Flags().GetBool("cuda-tensor")
+			rocm, _ := cmd.Flags().GetBool("rocm-hip")
+
+			if !cuda && !rocm {
+				return fmt.Errorf("no flags specified")
+			}
+
+			vendors, err := librarier.DetectGPUVendors()
+			if err != nil {
+				return fmt.Errorf("could not detect GPU vendors: %w", err)
+			}
+
+			hasAMD := containsVendor(vendors, library.AMD)
+			hasNVIDIA := containsVendor(vendors, library.NVIDIA)
+
+			if !hasAMD && !hasNVIDIA {
+				return fmt.Errorf("no AMD or NVIDIA GPU(s) detected...")
+			}
+
+			ctx := context.Background()
+
+			if cuda {
+				if !hasNVIDIA {
+					return fmt.Errorf("no NVIDIA GPU(s) detected...")
+				}
+
+				cudaOpts := ContainerOptions{
+					UseGPUs:    true,
+					Image:      "registry.gitlab.com/nunet/ml-on-gpu/ml-on-gpu-service/develop/pytorch",
+					Command:    []string{"python", "check-cuda-and-tensor-cores-availability.py"},
+					Entrypoint: []string{""},
+				}
+
+				images, err := docker.ImageList(ctx, types.ImageListOptions{})
+				if err != nil {
+					return fmt.Errorf("unable to list Docker images: %w", err)
+				}
+
+				if !imageExists(images, cudaOpts.Image) {
+					err := pullImage(ctx, docker, cudaOpts.Image, cmd.OutOrStdout())
+					if err != nil {
+						return fmt.Errorf("failed to pull CUDA image %s: %w", cudaOpts.Image, err)
+					}
+				}
+
+				err = runDockerContainer(ctx, docker, cudaOpts, cmd.OutOrStderr())
+				if err != nil {
+					return fmt.Errorf("failed to run CUDA container: %w", err)
+				}
+			}
+
+			if rocm {
+				if !hasAMD {
+					return fmt.Errorf("no AMD GPU(s) detected...")
+				}
+
+				rocmOpts := ContainerOptions{
+					Devices:    []string{"/dev/kfd", "/dev/dri"},
+					Groups:     []string{"video", "render"},
+					Image:      "registry.gitlab.com/nunet/ml-on-gpu/ml-on-gpu-service/develop/pytorch-amd",
+					Command:    []string{"python", "check-rocm-and-hip-availability.py"},
+					Entrypoint: []string{""},
+				}
+
+				images, err := docker.ImageList(ctx, types.ImageListOptions{})
+				if err != nil {
+					return fmt.Errorf("failed to list Docker images: %w", err)
+				}
+
+				if !imageExists(images, rocmOpts.Image) {
+					err := pullImage(ctx, docker, rocmOpts.Image, cmd.OutOrStdout())
+					if err != nil {
+						return fmt.Errorf("could not pull ROCm-HIP image %s: %w", rocmOpts.Image, err)
+					}
+				}
+
+				err = runDockerContainer(ctx, docker, rocmOpts, cmd.OutOrStderr())
+				if err != nil {
+					return fmt.Errorf("failed to run ROCm-HIP container: %w", err)
+				}
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().BoolVarP(&flagCudaTensor, "cuda-tensor", "c", false, "check CUDA Tensor")
+	cmd.Flags().BoolVarP(&flagRocmHip, "rocm-hip", "r", false, "check ROCM-HIP")
+
+	return cmd
 }
 
-var gpuCapacityCmd = &cobra.Command{
-	Use:    "capacity",
-	Short:  "Check availability of NVIDIA/AMD GPUs",
-	PreRun: isDMSRunning(),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		cuda, _ := cmd.Flags().GetBool("cuda-tensor")
-		rocm, _ := cmd.Flags().GetBool("rocm-hip")
+func runDockerContainer(ctx context.Context, docker Docker, options ContainerOptions, w io.Writer) error {
+	var (
+		err        error
+		id         string
+		config     *container.Config
+		hostConfig *container.HostConfig
+	)
 
-		if !cuda && !rocm {
-			return fmt.Errorf("no flags specified")
-		}
+	err = validateOptions(options)
+	if err != nil {
+		return fmt.Errorf("invalid container option: %w", err)
+	}
 
-		vendors, err := library.DetectGPUVendors()
-		if err != nil {
-			return fmt.Errorf("could not detect GPU vendors: %w", err)
-		}
+	config, hostConfig, err = configureContainer(options)
+	if err != nil {
+		return fmt.Errorf("failed to configure container: %w", err)
+	}
 
-		hasAMD := containsVendor(vendors, library.AMD)
-		hasNVIDIA := containsVendor(vendors, library.NVIDIA)
+	id, err = createAndStartContainer(ctx, docker, config, hostConfig, w)
+	if err != nil {
+		return fmt.Errorf("failed to create and start container %s: %w", id, err)
+	}
 
-		if !hasAMD && !hasNVIDIA {
-			return fmt.Errorf("no AMD or NVIDIA GPU(s) detected...")
-		}
+	err = attachAndMonitorContainer(ctx, docker, id, w)
+	if err != nil {
+		return fmt.Errorf("failed to attach and monitor container %s: %w", id, err)
+	}
 
-		ctx := context.Background()
-
-		if cuda {
-			if !hasNVIDIA {
-				return fmt.Errorf("no NVIDIA GPU(s) detected...")
-			}
-
-			cudaOpts := ContainerOptions{
-				UseGPUs:    true,
-				Image:      "registry.gitlab.com/nunet/ml-on-gpu/ml-on-gpu-service/develop/pytorch",
-				Command:    []string{"python", "check-cuda-and-tensor-cores-availability.py"},
-				Entrypoint: []string{""},
-			}
-
-			// initialize client outside of command
-			// pass as a field for the struct
-			cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-			if err != nil {
-				return fmt.Errorf("unable to create Docker client: %w", err)
-			}
-
-			images, err := cli.ImageList(ctx, types.ImageListOptions{})
-			if err != nil {
-				return fmt.Errorf("unable to list Docker images: %w", err)
-			}
-
-			if !imageExists(images, cudaOpts.Image) {
-				err := pullImage(ctx, cli, cudaOpts.Image, cmd.OutOrStdout())
-				if err != nil {
-					return fmt.Errorf("failed to pull CUDA image %s: %w", cudaOpts.Image, err)
-				}
-			}
-
-			err = runDockerContainer(ctx, cli, cudaOpts, cmd.OutOrStderr())
-			if err != nil {
-				return fmt.Errorf("failed to run CUDA container: %w", err)
-			}
-		}
-
-		if rocm {
-			if !hasAMD {
-				return fmt.Errorf("no AMD GPU(s) detected...")
-			}
-
-			rocmOpts := ContainerOptions{
-				Devices:    []string{"/dev/kfd", "/dev/dri"},
-				Groups:     []string{"video", "render"},
-				Image:      "registry.gitlab.com/nunet/ml-on-gpu/ml-on-gpu-service/develop/pytorch-amd",
-				Command:    []string{"python", "check-rocm-and-hip-availability.py"},
-				Entrypoint: []string{""},
-			}
-
-			cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-			if err != nil {
-				return fmt.Errorf("failed to create Docker client: %w", err)
-			}
-
-			images, err := cli.ImageList(ctx, types.ImageListOptions{})
-			if err != nil {
-				return fmt.Errorf("failed to list Docker images: %w", err)
-			}
-
-			if !imageExists(images, rocmOpts.Image) {
-				err := pullImage(ctx, cli, rocmOpts.Image, cmd.OutOrStdout())
-				if err != nil {
-					return fmt.Errorf("could not pull ROCm-HIP image %s: %w", rocmOpts.Image, err)
-				}
-			}
-
-			err = runDockerContainer(ctx, cli, rocmOpts, cmd.OutOrStderr())
-			if err != nil {
-				return fmt.Errorf("failed to run ROCm-HIP container: %w", err)
-			}
-		}
-
-		return nil
-	},
+	return nil
 }
 
 func validateOptions(options ContainerOptions) error {
 	if options.Image == "" {
 		return fmt.Errorf("image name cannot be empty")
+	}
+
+	if len(options.Command) == 0 {
+		return fmt.Errorf("command field cannot be empty")
+	}
+
+	if len(options.Entrypoint) == 0 {
+		return fmt.Errorf("entrypoint field cannot be empty")
 	}
 
 	return nil
@@ -183,32 +214,32 @@ func configureContainer(options ContainerOptions) (*container.Config, *container
 	return config, hostConfig, nil
 }
 
-func createAndStartContainer(ctx context.Context, cli *client.Client, config *container.Config, hostConfig *container.HostConfig, w io.Writer) (string, error) {
+func createAndStartContainer(ctx context.Context, docker Docker, config *container.Config, hostConfig *container.HostConfig, w io.Writer) (string, error) {
 	var (
 		resp container.ContainerCreateCreatedBody
 		err  error
 	)
 
-	resp, err = cli.ContainerCreate(ctx, config, hostConfig, nil, nil, "")
+	resp, err = docker.ContainerCreate(ctx, config, hostConfig, nil, nil, "")
 	if err != nil {
 		return "", fmt.Errorf("cannot create container: %v", err)
 	}
 
 	defer func() {
-		err = cli.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{})
+		err = docker.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{})
 		if err != nil {
 			fmt.Fprintf(w, "WARNING: could not remove container: %v\n", err)
 		}
 	}()
 
-	if err = cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+	if err = docker.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
 		return "", fmt.Errorf("cannot start container: %v", err)
 	}
 
 	return resp.ID, nil
 }
 
-func attachAndMonitorContainer(ctx context.Context, cli *client.Client, id string, w io.Writer) error {
+func attachAndMonitorContainer(ctx context.Context, docker Docker, id string, w io.Writer) error {
 	var (
 		out types.HijackedResponse
 		err error
@@ -217,7 +248,7 @@ func attachAndMonitorContainer(ctx context.Context, cli *client.Client, id strin
 		errCh  <-chan error
 	)
 
-	out, err = cli.ContainerAttach(ctx, id, types.ContainerAttachOptions{
+	out, err = docker.ContainerAttach(ctx, id, types.ContainerAttachOptions{
 		Stream: true,
 		Stdout: true,
 		Stderr: true,
@@ -228,7 +259,7 @@ func attachAndMonitorContainer(ctx context.Context, cli *client.Client, id strin
 
 	io.Copy(w, out.Reader)
 
-	waitCh, errCh = cli.ContainerWait(ctx, id, container.WaitConditionNotRunning)
+	waitCh, errCh = docker.ContainerWait(ctx, id, container.WaitConditionNotRunning)
 	select {
 	case waitResult := <-waitCh:
 		if waitResult.Error != nil {
@@ -236,37 +267,6 @@ func attachAndMonitorContainer(ctx context.Context, cli *client.Client, id strin
 		}
 	case err := <-errCh:
 		return fmt.Errorf("error waiting for container: %v", err)
-	}
-
-	return nil
-}
-
-func runDockerContainer(ctx context.Context, cli *client.Client, options ContainerOptions, w io.Writer) error {
-	var (
-		err        error
-		id         string
-		config     *container.Config
-		hostConfig *container.HostConfig
-	)
-
-	err = validateOptions(options)
-	if err != nil {
-		return fmt.Errorf("invalid container option: %w", err)
-	}
-
-	config, hostConfig, err = configureContainer(options)
-	if err != nil {
-		return fmt.Errorf("failed to configure container: %w", err)
-	}
-
-	id, err = createAndStartContainer(ctx, cli, config, hostConfig, w)
-	if err != nil {
-		return fmt.Errorf("failed to create and start container %s: %w", id, err)
-	}
-
-	err = attachAndMonitorContainer(ctx, cli, id, w)
-	if err != nil {
-		return fmt.Errorf("failed to attach and monitor container %s: %w", id, err)
 	}
 
 	return nil
@@ -283,11 +283,11 @@ func imageExists(images []types.ImageSummary, imageName string) bool {
 	return false
 }
 
-func pullImage(ctx context.Context, cli *client.Client, image string, w io.Writer) error {
+func pullImage(ctx context.Context, docker Docker, image string, w io.Writer) error {
 	ctxCancel, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	out, err := cli.ImagePull(ctxCancel, image, types.ImagePullOptions{})
+	out, err := docker.ImagePull(ctxCancel, image, types.ImagePullOptions{})
 	if err != nil {
 		return fmt.Errorf("unable to pull image %s: %v", image, err)
 	}
