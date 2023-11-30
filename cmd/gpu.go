3a8d8d4e55a1754df6ea44534b6205c2d42e53d5
Gustavo Silva <gustavo.silva@nunet.io>
2023-11-30 08:22:59 -0300

    refactor(cmd): define variables and values for cmds

--- cmd/gpu.go
+++ cmd/gpu.go
@@ -3,6 +3,28 @@ package cmd
 import "github.com/spf13/cobra"
 
 func init() {
+	var (
+		gpuControllerMap map[string]GPUController
+		gpuMap           map[string]GPU
+
+		gpuCapacityCmd *cobra.Command
+		gpuStatusCmd   *cobra.Command
+		gpuOnboardCmd  *cobra.Command
+	)
+
+	gpuControllerMap = map[string]GPUController{
+		"amd":    &AMDController{},
+		"nvidia": &NvidiaController{},
+	}
+	gpuMap = map[string]GPU{
+		"amd":    &amdGPU{},
+		"nvidia": &nvidiaGPU{},
+	}
+
+	gpuCapacityCmd = NewGPUCapacityCmd(librarier, docker)
+	gpuStatusCmd = NewGPUStatusCmd(librarier, executer, nvmlManager, gpuControllerMap, gpuMap)
+	gpuOnboardCmd = NewGPUOnboardCmd(utility, librarier, executer)
+
 	gpuCmd.AddCommand(gpuCapacityCmd)
 	gpuCmd.AddCommand(gpuStatusCmd)
 	gpuCmd.AddCommand(gpuOnboardCmd)
