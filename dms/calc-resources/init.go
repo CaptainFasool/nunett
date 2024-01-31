250d2a602975030547c127c31bb8d112c282c7a8
deependra.singh2 <deependra.singh@nunet.io>
2024-01-31 14:01:31 +0530

    fix: update calc-resources package

--- dms/calc-resources/init.go
+++ dms/calc-resources/init.go
@@ -1,4 +1,4 @@
-package telemetry
+package calc_resources
 
 import (
 	"gitlab.com/nunet/device-management-service/internal/logger"
@@ -7,5 +7,5 @@ import (
 var zlog *logger.Logger
 
 func init() {
-	zlog = logger.New("telemetry")
+	zlog = logger.New("calc-resources")
 }
