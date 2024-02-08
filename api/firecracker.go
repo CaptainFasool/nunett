edb842a3fba7d3546502dc3dd0ad3d3f70f60a1d
Gustavo Silva <gustavo.silva@nunet.io>
2024-02-08 11:40:16 -0300

    (api): separate concerns from firecracker handlers

---
+++ api/firecracker.go
@@ -0,0 +1,64 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gitlab.com/nunet/device-management-service/firecracker"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+)
+
+// HandleStartCustom godoc
+//
+//	@Summary		Start a VM with custom configuration.
+//	@Description	This endpoint is an abstraction of all primitive endpoints. When invokend, it calls all primitive endpoints in a sequence.
+//	@Tags			vm
+//	@Produce		json
+//	@Success		200
+//	@Router			/vm/start-custom [post]
+func HandleStartCustom(c *gin.Context) {
+	reqCtx := c.Request.Context()
+	span := trace.SpanFromContext(reqCtx)
+	span.SetAttributes(attribute.String("URL", "/vm/start-custom"))
+
+	var body firecracker.CustomVM
+	err := c.BindJSON(&body)
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	err = firecracker.StartCustom(reqCtx, body)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, gin.H{"message": "VM started successfully"})
+}
+
+// HandleStartDefault godoc
+//
+//	@Summary		Start a VM with default configuration.
+//	@Description	Everything except kernel files and filesystem file will be set by DMS itself.
+//	@Tags			vm
+//	@Produce		json
+//	@Success		200
+//	@Router			/vm/start-default [post]
+func HandleStartDefault(c *gin.Context) {
+	reqCtx := c.Request.Context()
+	span := trace.SpanFromContext(reqCtx)
+	span.SetAttributes(attribute.String("URL", "/vm/start-default"))
+
+	var body firecracker.DefaultVM
+	err := c.BindJSON(&body)
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	err = firecracker.StartDefault(reqCtx, body)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, gin.H{"message": "VM started successfully"})
+}
