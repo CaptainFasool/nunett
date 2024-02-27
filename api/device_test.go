5327f62a0611c2b34e06c3bbfe121b08500f41d0
Gustavo Silva <gustavo.silva@nunet.io>
2024-02-27 16:15:36 -0300

    (api): define device handlers

--- api/device_test.go
+++ api/device_test.go
@@ -7,8 +7,37 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"gitlab.com/nunet/device-management-service/utils"
 )
+
+func (h *MockHandler) DeviceStatusHandler(c *gin.Context) {
+	status := utils.RandomBool()
+	c.JSON(200, gin.H{"online": status})
+}
+
+func (h *MockHandler) ChangeDeviceStatusHandler(c *gin.Context) {
+	var status struct {
+		IsAvailable bool `json:"is_available"`
+	}
+	if c.Request.ContentLength == 0 {
+		c.JSON(400, gin.H{"error": "empty payload data"})
+		return
+	}
+	err := c.ShouldBindJSON(&status)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "invalid payload data"})
+		return
+	}
+	var msg string
+	if status.IsAvailable {
+		msg = "device set to online"
+	} else {
+		msg = "device status set to offline"
+	}
+	c.JSON(200, gin.H{"message": msg})
+}
 
 func TestDeviceStatusHandler(t *testing.T) {
 	router := SetupMockRouter()
@@ -27,19 +56,22 @@ func TestChangeDeviceStatusHandler(t *testing.T) {
 		description  string
 		payload      map[string]bool
 		expectedCode int
+		expectedMsg  string
 	}{
 		{
-			description:  "Change status to online",
+			description:  "change status to online",
 			payload:      map[string]bool{"is_available": true},
 			expectedCode: 200,
+			expectedMsg:  "device set to online",
 		},
 		{
-			description:  "Change status to offline",
+			description:  "change status to offline",
 			payload:      map[string]bool{"is_available": false},
 			expectedCode: 200,
+			expectedMsg:  "device set to offline",
 		},
 		{
-			description:  "Invalid payload",
+			description:  "invalid payload",
 			payload:      map[string]bool{},
 			expectedCode: 400,
 		},
@@ -52,5 +84,6 @@ func TestChangeDeviceStatusHandler(t *testing.T) {
 		router.ServeHTTP(w, req)
 
 		assert.Equal(t, tc.expectedCode, w.Code, tc.description)
+		assert.Equal(t, tc.expectedMsg, w.Body.String(), tc.description)
 	}
 }
