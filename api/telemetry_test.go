ddc46a4cce7ab822063bdbd79c657907e90bf5f9
Gustavo Silva <gustavo.silva@nunet.io>
2024-03-13 10:41:24 -0300

    (api): deprecate mock handlers for telemetry

--- api/telemetry_test.go
+++ api/telemetry_test.go
@@ -6,28 +6,12 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/nunet/device-management-service/models"
 )
 
-func (h *MockHandler) GetFreeResourcesHandler(c *gin.Context) {
-	free := models.FreeResources{
-		ID:                1,
-		TotCpuHz:          3400000000,
-		PriceCpu:          0.0005,
-		Ram:               16384,
-		PriceRam:          0.0001,
-		Vcpu:              8,
-		Disk:              500.0,
-		PriceDisk:         0.0002,
-		NTXPricePerMinute: 0.05,
-	}
-	c.JSON(200, free)
-}
-
 func TestGetFreeResourcesHandler(t *testing.T) {
-	router := SetupMockRouter()
+	router := SetupTestRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/api/v1/telemetry/free", nil)
