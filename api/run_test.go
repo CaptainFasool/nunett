80e5444597c10b7167448521422fc7b9ac3b7e1a
Gustavo Silva <gustavo.silva@nunet.io>
2024-03-13 10:40:25 -0300

    (api): deprecate mock handlers for run

--- api/run_test.go
+++ api/run_test.go
@@ -7,7 +7,6 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/nunet/device-management-service/models"
 )
@@ -28,52 +27,8 @@ type checkpoint struct {
 	LastModified  int64  `json:"last_modified"`
 }
 
-func (h *MockHandler) RequestServiceHandler(c *gin.Context) {
-	var depReq models.DeploymentRequest
-	err := c.BindJSON(&depReq)
-	if err != nil {
-		c.AbortWithStatusJSON(400, gin.H{"error": err.Error()})
-		return
-	}
-	resp := fundingRespToSPD{
-		ComputeProviderAddr: "foobar123",
-		EstimatedPrice:      12.34,
-		MetadataHash:        depReq.MetadataHash,
-		WithdrawHash:        depReq.WithdrawHash,
-		RefundHash:          depReq.RefundHash,
-		Distribute_50Hash:   depReq.Distribute_50Hash,
-		Distribute_75Hash:   depReq.Distribute_75Hash,
-	}
-	c.JSON(200, resp)
-}
-
-func (h *MockHandler) DeploymentRequestHandler(c *gin.Context) {
-	// original func do not return anything
-}
-
-func (h *MockHandler) ListCheckpointHandler(c *gin.Context) {
-	checkpoints := []checkpoint{
-		{
-			CheckpointDir: "/foo/bar/1",
-			FilenamePath:  "/foo/bar/baz.tar.gz",
-			LastModified:  1609459200,
-		},
-		{
-			CheckpointDir: "/foo/bar/2",
-			FilenamePath:  "/foo/bar/baz2.tar.gz",
-			LastModified:  1612137600,
-		},
-		{
-			CheckpointDir: "/foo/bar/3",
-			FilenamePath:  "/foo/bar/baz3.tar.gz",
-			LastModified:  1614556800,
-		},
-	}
-	c.JSON(200, checkpoints)
-}
-
 func TestRequestServiceHandler(t *testing.T) {
-	router := SetupMockRouter()
+	router := SetupTestRouter()
 
 	// fill depReq details
 	depReq := models.DeploymentRequest{}
@@ -87,7 +42,7 @@ func TestRequestServiceHandler(t *testing.T) {
 }
 
 func TestDeploymentRequestHandler(t *testing.T) {
-	router := SetupMockRouter()
+	router := SetupTestRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/api/v1/run/deploy", nil)
@@ -97,7 +52,7 @@ func TestDeploymentRequestHandler(t *testing.T) {
 }
 
 func TestListCheckpointHandler(t *testing.T) {
-	router := SetupMockRouter()
+	router := SetupTestRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/api/v1/run/checkpoints", nil)
