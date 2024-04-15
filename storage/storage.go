d4341d01357aa92c9d732d852b8ae6d1e533f5f6
joao.castro5 <joao.castro@nunet.io>
2024-04-15 15:10:15 -0300

    feat: storage package interface proposals

--- storage/storage.go
+++ storage/storage.go
@@ -1,150 +1,55 @@
+// Package storage handles mainly storage access to remote storage providers such as
+// AWS S3 and IPFS. Also, it handles the control of storage volumes.
 package storage
 
-/*
+import (
+	"gitlab.com/nunet/device-management-service/models"
+)
 
-Comments:
-1. This file considers storage interfaces dealing directly with volumes.
-Therefore, it's coupled with the file system.
-2. VolumeController is used by all versions
+type MiB uint64
 
-TODOs and questions:
-1. How to handle different storage provider params?
-    1.1. SpecConfig + InputSource or
-    1.2. Reader/Writer instantiated with different params for each implementation
+// StorageProvider handles I/O operations of files with remote storage providers
+// such as AWS S3 and IPFS.
+//
+// Its functionality is coupled with local mounted volumes, meaning that implementations
+// will rely on mounted files to upload data and downloading data will result in a
+// local mounted volume.
+//
+// - Authentication and availability-checking of storage provider are handled
+// when instantiating the implementation
+//
+// - Although it may be feasible to implement, the interface was not built with
+// the idea of supporting streaming of data and non-file storage operations (e.g.:
+// some databases)
+type StorageProvider interface {
+	// Upload uploads a storage volume data to a given remote storage provider.
+	// The operation results in a return value that might vary from provider to provider
+	// (and it may not exist in some cases).
+	Upload(vol StorageVolume, target *models.SpecConfig) (*models.SpecConfig, error)
 
-*/
+	// Download downloads data from a given source, mounting it to a certain local path.
+	// (Note: the operation will fail if the user running DMS does not have access permission
+	// to the given path).
+	Download(source *models.SpecConfig, outputPath string) (StorageVolume, error)
 
-// StorageVolume is basically the path to a directory/file where data is contained.
-// Plus some metadata.
-type StorageVolume struct {
-	Path            string
-	TimeLastUpdated int64
+	// Size returns the size of a given source. The method may also be useful to check
+	// if a given source is available.
+	Size(source *models.SpecConfig) (MiB, error)
 }
 
-// VolumeController is used to manage storage volumes. It'll probably be used
-// by storage provider interfaces.
+// StorageVolume contains mainly the path to a directory/file where data is mounted
+// and additional metadata.
+type StorageVolume struct {
+	// Path is the path to a directory/file where data is mounted
+	Path string
+
+	// Type is the type of the storage volume
+	Type string
+}
+
+// VolumeController is used to manage storage volumes which are data mounted to files/directories.
 type VolumeController interface {
 	CreateVolume(path string, volType string) (StorageVolume, error)
 	DeleteVolume(in StorageVolume) error
 	ListVolumes() ([]StorageVolume, error)
-	GetCapabilities(in StorageVolume) (map[string]interface{}, error)
-	GetVolumeStats(in StorageVolume) (map[string]interface{}, error)
-	CreateSnapshot(in StorageVolume) (StorageVolume, error)
-	DeleteSnapshot(snapshotID string) error
-	ListSnapshots() ([]StorageVolume, error)
 }
-
-//= = = = = = = = = =  VERSION 1: InputSource/OutputTarget  = = = = = = = = = =
-
-/*
-
-Pros:
-- It may be easier to understand the code at first. StorageProvider interface is not
-hard to understand or use it.
-- One unique implementation for both upload/download operations and other secundary operations
-
-Cons:
-- It does not have compile-time type checking as it relies on `map[string]interfaces{}` to be
-decoded by the caller based on a given type (as executor is doing)
-- Although the interface is easier to understand, having this general type such as InputSource/OutputTarget
-makes the params harder to understand because they're not directly coupled with the interface. Each implementation
-will use their own decoder.
-*/
-
-// InputSource/StoreResult (temporary) will be used by different storage type implementations
-// which need different params when reading/writing from/to them.
-// (similar to SpecConfig used by the Executor)
-type InputSource map[string]interface{} // basically *SpecConfig
-type OutputTarget map[string]interface{}
-
-// StoreResult as described above + contains metadata returned by the store operation
-type StoreResult map[string]interface{}
-
-/*
-Job's input: from S3, IPFS, etc.
-Job's output stored on S3, IPFS, etc.
-*/
-
-// StorageProvider: I/O as files of remote storage provider (not necessarily: streaming, enabling databases)
-//
-// - Authentication done when instantiating the implementation
-//   - CONCERN: different files might have different auth within the same account
-//
-// - IsAvailable() checked when instantiating the implementation
-type StorageProvider interface {
-	// Upload uploads the data to the source based on StorageVolume and
-	// the target of operation
-	//
-	// CONCERN: do we really need to get returned StoreResult?
-	// I'm afraid we might need to get any metadata returned after store operation
-	// for some cases
-	Upload(input StorageVolume, target OutputTarget) (StoreResult, error)
-
-	// Download downloads the data from the source
-	Download(input InputSource, outputPath string) (StorageVolume, error)
-
-	// Size is also used to check if it exists
-	Size(identifier string) (int64, error)
-}
-
-//= = = = = = = = = =  VERSION 2: StorageReader/Writer  = = = = = = = = = =
-
-/*
-
-This version eliminates the need of the InputSource/OutputTarget.
-
-Pros:
-- Compile-time type checking
-- Modular interfaces
-
-Cons:
-- It may be harder to understand at first.
-- Unusual way of dealing with storage provider maybe?
-
-*/
-
-// StorageReader: when instantiating the implementation, the reader will contain
-// information regarding necessary params to read from the source such as: key,
-// paths, auth, etc.
-type StorageReader interface {
-	// Download downloads the data from the source
-	Download(outputPath string) (StorageVolume, error)
-
-	// Size is also used to check if it exists
-	Size() (int64, error)
-}
-
-// StorageWriter: when instantiating the implementation, the writer will contain
-// information regarding necessary params to write to the source such as: key,
-// paths, auth, etc.
-type StorageWriter interface {
-	// Upload uploads the data to the source based on the information inputed when
-	// the writer was instantiated
-	Upload(in StorageVolume) (StoreResult, error)
-}
-
-// [] mounting or not mounting
-// mounting
-
-// [] enable streaming
-// interface will not able necessarily support streaming
-
-// TODOs:
-// think on StorageVolume structure
-// review VolumeController
-// review other interfaces, final version
-
-// To decide tomorrow:
-
-// [] decide which version
-// 1
-
-// [] decide where to keep encryption part
-// outside of it
-
-// Solved:
-// [] add Delete()/Remove()?
-// not have those
-
-// [] add Update()?
-// not have this
