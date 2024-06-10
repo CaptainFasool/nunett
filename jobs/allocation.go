38dcaf3249bdfc618e000ef8dd5e05093ddc1a5b
Gustavo Silva <gustavo.silva@nunet.io>
2024-06-10 15:46:51 -0300

    initial interface for allocation

---
+++ jobs/allocation.go
@@ -0,0 +1,80 @@
+// Package jobs deals with everything job-related
+package jobs
+
+import (
+	"context"
+	"fmt"
+
+	"gitlab.com/nunet/device-management-service/executor"
+	"gitlab.com/nunet/device-management-service/models"
+)
+
+// a jobspec will be transformed in one or multiple jobs
+type job struct {
+	id   string
+	reqs []*models.ExecutionRequest // id of each request match job id
+	executor.Executor
+}
+
+func (j *job) ExecuteAll(ctx context.Context) {
+	// add logic for concurrent dispatch
+	for _, req := range j.reqs {
+		j.Run(ctx, req)
+	}
+}
+
+// Allocation maps an execution request to a given node
+type Allocation struct {
+	ID      string
+	NodeID  string
+	Request *models.ExecutionRequest
+}
+
+// NewAllocation returns a pointer to *Allocation
+func NewAllocation(nodeID string, req *models.ExecutionRequest) *Allocation {
+	return &Allocation{
+		ID:      "teste",
+		NodeID:  nodeID,
+		Request: req,
+	}
+}
+
+// Allocater manages allocations
+type Allocater interface {
+	Allocate(ctx context.Context, alloc *Allocation)
+}
+
+// Allocator is a form of collector for allocations
+// It implements the Allocater interface
+type Allocator struct {
+	ch chan *Allocation
+}
+
+// NewAllocator returns a pointer to allocator struct
+func NewAllocator() *Allocator {
+	return &Allocator{
+		ch: make(chan *Allocation),
+	}
+}
+
+// Listen wait on a *Allocation channel until it receives a value
+// Once a value is read, it calls Allocate method
+func (a *Allocator) Listen(ctx context.Context) {
+	for {
+		go func() {
+			select {
+			case <-ctx.Done():
+				fmt.Println("stopping listen...")
+				break
+			case alloc := <-a.ch:
+				a.Allocate(ctx, alloc)
+			}
+		}()
+	}
+}
+
+// Allocate currently implements a placeholder text
+// Here we should define the mechanism to actually place the allocation (e.g. RPC calls, libp2p streams etc.)
+func (a *Allocator) Allocate(_ context.Context, alloc *Allocation) {
+	fmt.Printf("allocation %s: placing execution %s at node %s", alloc.ID, alloc.Request.ExecutionID, alloc.NodeID)
+}
