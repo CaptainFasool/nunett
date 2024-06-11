79bd9b53a2f9f9765ceb66c4c5d8fd4bf0329401
joao.castro5 <joao.castro@nunet.io>
2024-06-11 13:09:17 -0300

    feat(libp2p): final tests for private swarm

--- network/libp2p/pnet_test.go
+++ network/libp2p/pnet_test.go
@@ -5,35 +5,60 @@ import (
 	"testing"
 	"time"
 
-	// "github.com/spf13/afero"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWithSwarmKey(t *testing.T) {
-	numNodes := 5
+// TestPrivateSwarm tests connectivity between nodes in the same private swarm and
+// it checks if connection trials with outside nodes fail as it should.
+//
+// TODO: sometimes it takes several seconds to run this test (maximum 15s)
+func TestPrivateSwarm(t *testing.T) {
+	numNodes := 3
 	nodes := createTestNetwork(t, numNodes, true)
 
-	// test if peers within the private network can ping each other
 	for _, nodeOuter := range nodes {
 		for _, nodeInner := range nodes {
-			res, err := nodeOuter.Ping(context.TODO(), nodeInner.Host.ID().String(), 5*time.Second)
-			assert.NoError(t, err)
-			assert.Equal(t, true, res.Success)
+			t.Run("ping between peers within the same private swarm", func(t *testing.T) {
+				t.Parallel()
+				if nodeInner.Host.ID() == nodeOuter.Host.ID() {
+					return
+				}
+				res, err := nodeOuter.Ping(context.TODO(), nodeInner.Host.ID().String(), 5*time.Second)
+				assert.NoError(t, err)
+				assert.Equal(t, true, res.Success)
+			})
 		}
 	}
 
-	// TODO: instantiate a new peer without the swarm key and see if it
-	// can ping peers within the private network
-	// 	bootstrapMultiAddr, err := nodes[0].GetMultiaddr()
-	// 	assert.NoError(t, err)
-	//
-	// 	config := setupLibp2pConfig(t, 64400, bootstrapMultiAddr, false)
-	// 	p, err := New(config, afero.NewMemMapFs())
-	// 	assert.NoError(t, err)
-	//
-	// 	err = p.Init(context.TODO())
-	// 	assert.NoError(t, err)
-	//
-	// 	err = p.Start(context.TODO())
-	// 	assert.NoError(t, err)
+	bootstrapMultiAddr, err := nodes[0].GetMultiaddr()
+	assert.NoError(t, err)
+
+	config := setupPeerConfig(t, 64400, bootstrapMultiAddr, false)
+	p, err := New(config, afero.NewMemMapFs())
+	assert.NoError(t, err)
+
+	err = p.Init(context.TODO())
+	assert.NoError(t, err)
+
+	err = p.Start(context.TODO())
+	assert.NoError(t, err)
+
+	// Ping seems to rely on peerIDs which under the hood are then used to search for peers multiaddresses
+	// which might fail if the peer has no access to this info through DHT or local information (peerstore).
+	// A more reliable way to test the security of a private network is to try to dial peers directly
+	// using their multiaddresses.
+	for _, node := range nodes {
+		t.Run("Connection trial with peer outside the private swarm", func(t *testing.T) {
+			t.Parallel()
+			// trial: outer peer trying to connect with a peer within a pnet
+			err := p.Host.Connect(context.TODO(), peer.AddrInfo{ID: node.Host.ID(), Addrs: node.Host.Addrs()})
+			assert.Error(t, err)
+
+			// trial: peer within a private network trying to establish a connection with an outer peer
+			err = node.Host.Connect(context.TODO(), peer.AddrInfo{ID: p.Host.ID(), Addrs: p.Host.Addrs()})
+			assert.Error(t, err)
+		})
+	}
 }
